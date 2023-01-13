de81e436eb32d7b3d5a2dbbb4ac38e9e8a646db8
LuisCodeForonda <luisalbertoforondac@gmail.com>
2023-01-12 20:36:12 -0400

    fucion django html

--- proyectoctv/ctv/urls.py
+++ proyectoctv/ctv/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('quienessomos/', views.quienes_somos, name="quienessomos"),
+    path('programacion/', views.programacion, name="programacion"),
+    path('programas/', views.programas, name="programas"),
+    path("contactos/", views.contactos, name="contactos"),
 ]
--- proyectoctv/ctv/views.py
+++ proyectoctv/ctv/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("pagina de index")
+    return render(request, "ctv/index.html", {})
+
+def quienes_somos(request):
+    return render(request, "ctv/quienessomos.html", {})
+
+def programacion(request):
+    return render(request, "ctv/programacion.html", {})
+
+def programas(request):
+    return render(request, "ctv/programas.html", {})
+
+def contactos(request):
+    return render(request, "ctv/contactos.html", {})
+
