55d5b51d489f7bcede77958662aeef22d0a0ea88
luis <luisabertoforondac@gamil.com>
2023-01-17 17:47:08 -0400

    base de datos cambiada a mysql

--- proyectoctv/ctv/admin.py
+++ proyectoctv/ctv/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Programacion
 # Register your models here.
 
-admin.site.register(Programacion)
+#admin.site.register(Programacion)
+@admin.register(Programacion)
+class ProgramacionAdmin(admin.ModelAdmin):
+    #listar los datos
+    list_display = ('hora_emision', 'nombre', 'dias')
+
+    #busqueda por nombre
+    search_fields = ('nombre',)
+    list_display_links = ('hora_emision', 'nombre')
